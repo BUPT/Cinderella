4eb5bd8f6ae0788c8446eb43f90773a2f6063a66
哈哈小蜡烛 <paperbox541@icloud.com>
2016-02-29 17:19:10 +0800

    add some explain

--- training/mavenDemo/src/main/java/com/qq/IbotInfo.java
+++ training/mavenDemo/src/main/java/com/qq/IbotInfo.java
@@ -41,14 +41,14 @@ public class IbotInfo extends HttpServlet {
      * Default constructor. 
      */
     public IbotInfo() {
-        // TODO Auto-generated constructor stub
+ 
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+
 		String str="please use post method";
 		response.getOutputStream().write(str.toString().getBytes("UTF-8"));  				
 	}
@@ -57,19 +57,18 @@ public class IbotInfo extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
 		request.setCharacterEncoding("utf-8");
 		
-		EmailInput email=new EmailInput();
-		BotResult botResult=new BotResult();
-		String Raw_filename="D:\\iBotest\\bizplan1.txt";
-		String Abstract_filename="D:\\iBotest\\abstract.txt";
+		EmailInput email = new EmailInput();
+		BotResult botResult = new BotResult();
+		String filename = "D:\\iBotest\\bizplan1.txt";
 		
-		String sender=null;
-		String receiver=null; 
-        String sendtime=null;
-        String subject=null;
-        String body=null;         
+		String sender = null;
+		String receiver = null; 
+        String sendtime = null;
+        String subject = null;
+        String body = null;         
         ArrayList<String> attachTemp = new ArrayList<String> ();
          
 		if(request.getHeader("Content-Type").indexOf("multipart/form-data")!=-1)
@@ -84,20 +83,19 @@ public class IbotInfo extends HttpServlet {
 			SmartUpload su=new SmartUpload();
 			su.initialize(getServletConfig(), request, response);
 			com.jspsmart.upload.Request req = su.getRequest();
-			JSONObject obj = null;
-			String revdata=null;
+			
 			try 
 			{
 				su.upload();
-				sender=req.getParameter("sender");
-				receiver=req.getParameter("receiver"); 
-				sendtime=req.getParameter("sendtime");
-				subject=req.getParameter("subject");
-				body=req.getParameter("body");
-				int count=su.save(realPath);
-				for(int i=0;i<su.getFiles().getCount();i++)
+				sender = req.getParameter("sender");
+				receiver = req.getParameter("receiver"); 
+				sendtime = req.getParameter("sendtime");
+				subject = req.getParameter("subject");
+				body = req.getParameter("body");
+				int count = su.save(realPath);
+				for(int i = 0;i<su.getFiles().getCount();i++)
 				{
-					com.jspsmart.upload.File tempFile=su.getFiles().getFile(i);
+					com.jspsmart.upload.File tempFile = su.getFiles().getFile(i);
 					System.out.println(realPath+"\\"+tempFile.getFileName());
 					attachTemp.add(realPath+"\\"+tempFile.getFileName());
 				}
@@ -120,21 +118,21 @@ public class IbotInfo extends HttpServlet {
 		    int i=0;
 		    if(j1!=-1 && j2!=-1)
 		    {
-		    	sender=json.substring(j1+7, j2-1);
+		    	sender = json.substring(j1+7, j2-1);
 		    }
 		    if(j2!=-1 && j3!=-1)
 		    {
-		    	receiver=json.substring(j2+9, j3-1);
+		    	receiver = json.substring(j2+9, j3-1);
 		    }
 		    if(j3!=-1 && j4!=-1)
 		    {
-		    	sendtime=json.substring(j3+9, j4-1);
+		    	sendtime = json.substring(j3+9, j4-1);
 		    }
 		    if(j4!=-1 && j5!=-1)
 		    {
-		    	subject=json.substring(j4+8, j5-1);
+		    	subject = json.substring(j4+8, j5-1);
 		    }
-		    body=json.substring(j5+5,json.length()-1);
+		    body = json.substring(j5+5,json.length()-1);
 		}
 				
 		email.setSender(sender);
@@ -144,29 +142,27 @@ public class IbotInfo extends HttpServlet {
 		email.setEmailBody(body);
 		email.setEmailAttach(attachTemp);
 		
-		MailTxtInput(Raw_filename,email);
+		MailTxtInput(filename,email);
 		
 		try {
-			botResult=GetKeyPart(Raw_filename,botResult);
+			botResult = GetKeyPart(filename,botResult);
+			botResult = GetOtherPart(filename,botResult);
 		} catch (UnirestException e1) {
-			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-		
-		try {
-			botResult=GetOtherPart(Raw_filename,botResult);
-		} catch (UnirestException | JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+				
 		try {
 			iBotOutput(botResult,response);
 		} catch (JSONException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
+	/**
+	 * 从request中提取出采用curl -d 传输的json信息并进行UTF-8编码
+	 * @param request
+	 * @return json信息字符串
+	 * @throws IOException
+	 */
 	public String readJSONString(HttpServletRequest request) throws IOException
 	{
 		 BufferedReader br = new BufferedReader(new InputStreamReader(request.getInputStream()));
@@ -175,32 +171,36 @@ public class IbotInfo extends HttpServlet {
 	        while((line = br.readLine())!=null){
 	            sb.append(line);
 	        }
-	        // 将资料解码
 	        String reqBody = sb.toString();
 	        return URLDecoder.decode(reqBody, "UTF_8");
     }
+	/**
+	 * 从email对象中获取邮件信息，并以txt格式存储
+	 * @param fileName 存储邮件信息的文件名
+	 * @param email email对象
+	 */
 	public void MailTxtInput(String fileName,EmailInput email)
 	{
-		String sendInfo=email.getSender();
-		String receiveInfo=email.getReceiver();
-		String timeInfo=email.getSendTime();
-		String subjectInfo=email.getEmailSubject();
-		String bodyInfo=email.getEmailBody();
+		String sendInfo = email.getSender();
+		String receiveInfo = email.getReceiver();
+		String timeInfo = email.getSendTime();
+		String subjectInfo = email.getEmailSubject();
+		String bodyInfo = email.getEmailBody();
 		ArrayList<String> appendInfo = email.getEmailAttach();
 		Tika tika = new Tika();
 		String text = "";
 		if(appendInfo.isEmpty())
 		{
-			text="您没有指定任何附件";
+			text = "您没有指定任何附件";
 			System.out.println(text);
 		}
 		else
 		{
 			try 
 			{	
-				for(int i=0;i<appendInfo.size();i++)
+				for(int i = 0;i<appendInfo.size();i++)
 				{
-					File file=new File(appendInfo.get(i));
+					File file = new File(appendInfo.get(i));
 					if(!file.exists())  
 					{
 						text = text+"您指定的第"+(i+1)+"个附件不存在!\n";
@@ -223,7 +223,7 @@ public class IbotInfo extends HttpServlet {
 		}
 		try
 		{
-			String filename=fileName;	
+			String filename = fileName;	
 			File file = new File(filename);
 		    if(!file.exists()) 
 		    {
@@ -247,6 +247,119 @@ public class IbotInfo extends HttpServlet {
 		}			
 		
 	}
+	
+	/**
+	 * 获取BP语料的主要领域BizArea
+	 * @param bot
+	 * @throws UnirestException 
+	 * @throws IOException 
+	 */
+	public  BotResult GetKeyPart(String infileName,BotResult bot) throws IOException, UnirestException
+	{
+		String SENTIMENT_URL = "http://api.bosonnlp.com/keywords/analysis";
+		String AnalyseLine = Txt2Abstract(infileName,SENTIMENT_URL);
+		
+		AnalyseLine = AnalyseLine.replace("[","").replace("]","");	
+		String[] all = AnalyseLine.split(",");
+		
+		ArrayList<String> bizArea = new ArrayList<String> ();
+		if(all.length >= 4)
+		{
+			bizArea.add(all[1].replace("\"", ""));
+			bizArea.add(all[3].replace("\"", ""));
+		}
+		else if(all.length >= 2&&all.length < 4)
+		{
+			bizArea.add(all[1].replace("\"", ""));
+		}
+		else
+		{
+			bizArea.add("null");
+		}
+		bot.setBizArea(bizArea);
+		return bot;
+				
+	}
+	/**
+	 * 获取BP语料的其他信息，包括Location、founderName、company_name、product_name
+	 * @param infileName
+	 * @param bot
+	 * @return
+	 * @throws IOException
+	 * @throws UnirestException
+	 * @throws JSONException
+	 */
+	public BotResult GetOtherPart(String infileName,BotResult bot) throws IOException, UnirestException, JSONException
+	{
+		String SENTIMENT_URL="http://api.bosonnlp.com/ner/analysis";
+		String AnalyseLine = Txt2Abstract(infileName,SENTIMENT_URL);
+		
+		JSONArray jsonArray = new JSONArray(AnalyseLine);
+		
+		String entity=null;
+		String word=null;
+		
+		for (int i = 0; i < jsonArray.length(); i++) 
+		{
+			JSONObject jsonObj = jsonArray.getJSONObject(i);
+			word = jsonObj.get("word").toString();
+			entity = jsonObj.get("entity").toString();
+		}
+		
+		entity = entity.replace("[", "").replace("]", "").replace("\"", "");
+		String[] all_entity = entity.split(",");
+		
+		
+		word = word.replace("[", "").replace("]", "").replace("\"", "");
+		String[] all_word = word.split(",");
+		
+		int locationFlag = 0,cnameFlag = 0,pronameFlag = 0;
+		int temp = 0;
+		ArrayList<String> founderName = new ArrayList<String> ();
+		
+		for(int i = 0;i<all_entity.length;i++)
+		{
+			if(all_entity[i].equals("location")&&locationFlag==0)
+			{
+				locationFlag = 1;
+				temp = Integer.parseInt(all_entity[i-2]);
+				bot.setLocation(all_word[temp]);
+			}
+			else if(all_entity[i].equals("person_name"))
+			{
+				temp = Integer.parseInt(all_entity[i-2]);
+				founderName.add(all_word[temp]);	
+			}
+			else if(all_entity[i].equals("company_name")&&cnameFlag==0)
+			{
+				cnameFlag = 1;
+				temp = Integer.parseInt(all_entity[i-2]);
+				bot.setCompanyName(all_word[temp]);
+			}
+			else if(all_entity[i].equals("product_name")&&pronameFlag==0)
+			{
+				pronameFlag = 1;
+				temp = Integer.parseInt(all_entity[i-2]);
+				bot.setProjectName(all_word[temp]);
+			}
+			
+		}
+		
+		int financeLimit = 30000;
+		String tranStock = "20%";
+		bot.setFounderName(founderName);
+		bot.setFinanceLimit(financeLimit);
+		bot.setTranStock(tranStock);
+		return bot;
+	}
+	/**
+	 * 将txt信息转化成字符串发送给BosonNLP进行处理
+	 * @param infileName 存储邮件信息的文件名
+	 * @param SENTIMENT_URL BosonNLP的API地址
+	 * @return
+	 * @throws IOException
+	 * @throws UnirestException
+	 */
 	public String Txt2Abstract(String infileName,String SENTIMENT_URL) throws IOException, UnirestException 
 	{
 		File file = new File(infileName);
@@ -261,128 +374,31 @@ public class IbotInfo extends HttpServlet {
             	result += line;
             }    
             read.close();
-        }
-		
-		String body = "[\""+result+"\"]";    
-		
-		System.out.println("body:"+body);
-		
+        }		
+		String body = "[\""+result+"\"]";    		
+			
 		HttpResponse <JsonNode> jsonResponse = Unirest.post(SENTIMENT_URL)
 			.header("Content-Type", "application/json")
             .header("Accept", "application/json")
             .header("X-Token", "5mR6aTnx.4451.Gx2Jt_BBGdFE")
             .body(body)
-            .asJson();
-			
-		String finalStr = jsonResponse.getBody().toString();
-		//Unirest.shutdown();			
+            .asJson();			
+		String finalStr = jsonResponse.getBody().toString();		
         return finalStr;
 	}
 	/**
-	 * 
+	 * 以json数组的形式返回处理结果
 	 * @param bot
-	 * @throws UnirestException 
-	 * @throws IOException 
+	 * @param response
+	 * @throws JSONException
 	 */
-	public  BotResult GetKeyPart(String infileName,BotResult bot) throws IOException, UnirestException
-	{
-		String SENTIMENT_URL="http://api.bosonnlp.com/keywords/analysis";
-		String AnalyseLine = Txt2Abstract(infileName,SENTIMENT_URL);
-		
-		AnalyseLine=AnalyseLine.replace("[","").replace("]","");	
-		String[] all=AnalyseLine.split(",");
-		
-		ArrayList<String> bizArea=new ArrayList<String> ();
-		if(all.length>=4)
-		{
-			bizArea.add(all[1].replace("\"", ""));
-			bizArea.add(all[3].replace("\"", ""));
-		}
-		else if(all.length>=2&&all.length<4)
-		{
-			bizArea.add(all[1].replace("\"", ""));
-		}
-		else
-		{
-			bizArea.add("null");
-		}
-		bot.setBizArea(bizArea);
-		return bot;
-				
-	}
-	public BotResult GetOtherPart(String infileName,BotResult bot) throws IOException, UnirestException, JSONException
-	{
-		String SENTIMENT_URL="http://api.bosonnlp.com/ner/analysis";
-		String AnalyseLine = Txt2Abstract(infileName,SENTIMENT_URL);
-		
-		JSONArray jsonArray = new JSONArray(AnalyseLine);
-		int iSize = jsonArray.length();
-		String entity=null;
-		String word=null;
-		
-		for (int i = 0; i < iSize; i++) 
-		{
-			JSONObject jsonObj = jsonArray.getJSONObject(i);
-			word = jsonObj.get("word").toString();
-			entity = jsonObj.get("entity").toString();
-		}
-		
-		entity = entity.replace("[", "").replace("]", "").replace("\"", "");
-		String[] all_entity = entity.split(",");
-		
-		
-		word = word.replace("[", "").replace("]", "").replace("\"", "");
-		String[] all_word = word.split(",");
-		
-		int locationFlag=0,cnameFlag=0,pronameFlag=0;
-		int temp=0;
-		ArrayList<String> founderName=new ArrayList<String> ();
-		
-		for(int i=0;i<all_entity.length;i++)
-		{
-			if(all_entity[i].equals("location")&&locationFlag==0)
-			{
-				locationFlag=1;
-				temp= Integer.parseInt(all_entity[i-2]);
-				bot.setLocation(all_word[temp]);
-			}
-			else if(all_entity[i].equals("person_name"))
-			{
-				temp= Integer.parseInt(all_entity[i-2]);
-				founderName.add(all_word[temp]);	
-			}
-			else if(all_entity[i].equals("company_name")&&cnameFlag==0)
-			{
-				cnameFlag=1;
-				temp= Integer.parseInt(all_entity[i-2]);
-				bot.setCompanyName(all_word[temp]);
-			}
-			else if(all_entity[i].equals("product_name")&&pronameFlag==0)
-			{
-				pronameFlag=1;
-				temp= Integer.parseInt(all_entity[i-2]);
-				bot.setProjectName(all_word[temp]);
-			}
-			
-		}
-		
-		int financeLimit=30000;
-		String tranStock="20%";
-		bot.setFounderName(founderName);
-		bot.setFinanceLimit(financeLimit);
-		bot.setTranStock(tranStock);
-		return bot;
-	}
-	
 	public void iBotOutput(BotResult bot,HttpServletResponse response) throws JSONException
 	{
-		try {
-		
+		try {		
 			response.reset();
 			response.setCharacterEncoding("UTF-8");
 			response.setContentType("text/json; charset=UTF-8"); 
 			JSONStringer stringer = new JSONStringer();  
-
 			try {
 				 stringer.object().key("city").value(bot.getLocation()).  
 			        key("startup").value(bot.getProjectName()).  
@@ -395,15 +411,12 @@ public class IbotInfo extends HttpServlet {
 				response.getOutputStream().write(stringer.toString().getBytes("UTF-8"));  			
 			} 
 			catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
 		catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
 	}
 }
 
