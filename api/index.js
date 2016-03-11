8e1f13fcea1aa92604084dd3f60fa0c216266999
哈哈小蜡烛 <paperbox541@icloud.com>
2016-03-11 16:46:16 +0800

    fix error

---
+++ api/index.js
@@ -0,0 +1,31 @@
+/**
+ * Module dependencies.
+ */
+var express = require('express');
+var routes = require('../material/All2Txt');
+var http = require('http');
+var path = require('path');
+var app = express();
+
+// all environments参数设置
+app.set('port', process.env.PORT || 8080);
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.bodyParser());
+
+
+// development only app.use启用大量的中间件
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+//是一个路由控制器
+//app.get('/', routes.index);
+app.post('/ibot', routes.getInfo);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
