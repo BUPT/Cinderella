2dae4b201e084fc2bbb6dc83b5ee2fa09ab28f0d
哈哈小蜡烛 <paperbox541@icloud.com>
2016-03-22 14:53:10 +0800

    add needle to send http post to get txt of attachment

--- material/All2Txt.js
+++ material/All2Txt.js
@@ -5,6 +5,7 @@ var fs = require("fs");
 var util = require("util");
 var Txt2Meta = require("../training/txt2meta");
 var formidable = require('formidable');
+var needle = require('needle');
 
 exports.getInfo = function(req, res){    
     var contentType = req.get('Content-Type');    	
@@ -36,10 +37,27 @@ function DealMultiData(req,res)
     form.keepExtensions = true; 
     form.maxFieldsSize = 2 * 1024 * 1024;
     
+    
+    
+    
     form.on('file',function(name, file) {
     	console.log("file:"+name);
     	console.log(file.path);
     	fs.rename(file.path, form.uploadDir + file.name);
+    	
+    	var data = {
+		  file:{file:form.uploadDir + file.name, content_type:'application/msword'},
+		  apikey:'c173872b-5938-457f-9083-332c418ccf25',
+		  extract_metadata:'false',
+		  extract_text:'true',
+		  extract_xmlattributes:'false'
+		};
+		needle.post('https://api.havenondemand.com/1/api/sync/extracttext/v1', data, { multipart: true }, function(err, resp, body) 
+		{
+			if (!err && resp.statusCode == 200)
+		    console.log(resp.body.document[0].content); // here you go, mister. 
+		});
+		
 	});
     
     form.on('error', function(err) {
@@ -54,8 +72,11 @@ function DealMultiData(req,res)
 		var body = fields.body || 'none';	
 		var data = "发件人:"+sender+'\n'+"收件人："+receiver+'\n'+"发件时间："+sendtime+'\n'+"邮件主题："+subject+'\n'+"邮件正文："+body+'\n';			
 		console.log(data);
-//		Txt2Meta.txt2Meta(data, res);
+		
+//		Txt2Meta.txt2Meta(data, res);on function
 		res.writeHead(200, {'content-type': 'text/html'});
   		res.end("hello!");
-  	}); 
+  	});
+  	
+  	
 };
