a3c33b1b0e24b6bc4a7f55aceb347ed1c95abb38
哈哈小蜡烛 <paperbox541@icloud.com>
2016-06-15 16:56:39 +0800

    add the test part if body equals to "CINDERELLA"

--- src/main/java/com/qq/GetInfo.java
+++ src/main/java/com/qq/GetInfo.java
@@ -14,8 +14,10 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -54,14 +56,13 @@ public class GetInfo extends HttpServlet {
      * Default constructor. 
      */
     public GetInfo() {
-        // TODO Auto-generated constructor stub
+      
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doPost(request, response);
 	}
 
@@ -100,35 +101,54 @@ public class GetInfo extends HttpServlet {
 		else if(request.getHeader("Content-Type").indexOf("application/json")!=-1)
 		{	
 			email = RevJsonData(email,request);		    
-		}
-		else if(request.getHeader("Content-Type").indexOf("application/x-www-form-urlencoded")!=-1 )
-		{
-			email = RevFormData(email,request);
-		}
+		}     
 		try 
 		{
 			MailTxtInput(filename,email);
+			if(email.getEmailBody().equals("CINDERELLA"))
+		    {
+				File fileTemp = new File(filename);
+				String sInput = "";
+				if(fileTemp.isFile() && fileTemp.exists())
+				{ 
+		            InputStreamReader read = new InputStreamReader (new FileInputStream(fileTemp),"utf-8");
+		            BufferedReader in = new  BufferedReader(read);   
+		            String line; 
+		            while((line = in.readLine())!=null) 
+		            {   
+		            	line += "\r\n";
+		            	sInput += line;
+		            }    
+		            read.close();
+		        }	
+//				System.out.println(sInput);
+				iBot_Test_Output(sInput,response);	
+	        }
+	        else
+	        {
+	        	try 
+	        	{
+	    			String botResult = DealNLP(filename);
+	    			iBotOutput(botResult,response);
+	    		} 
+	        	catch (InterruptedException e) 
+	        	{
+	    			e.printStackTrace();
+	    		}	   		
+	        }
 		} 
 		catch (TikaException e) {
 			e.printStackTrace();
-		}	
-		
-		try {
-			String botResult = DealNLP(filename);
-			iBotOutput(botResult,response);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}	   		
+		}			
 	}
 	
-	public EmailInput RevMultiData(EmailInput email,HttpServletRequest request, HttpServletResponse response,String realPath) throws UnsupportedEncodingException{
+	public EmailInput RevMultiData(EmailInput email,HttpServletRequest request, HttpServletResponse response,String realPath) throws IOException{
 		
-		String sender = null;
-		String receiver = null; 
-        String sendtime = null;
-        String subject = null;
-        String body = null;         
+		String sender = "";
+		String receiver = ""; 
+        String sendtime = "";
+        String subject = "";
+        String body = "";         
         ArrayList<String> attachTemp = new ArrayList<String> ();
  
         int sizeThreshold=1024*1024; //缓存区大小
@@ -147,28 +167,52 @@ public class GetInfo extends HttpServlet {
             fileItems = servletFileUpload.parseRequest(request);
             for(FileItem fileItem:fileItems)
             {
-                String filePath = fileItem.getName();         
-                if(filePath==null || filePath.trim().length()==0)
-                    continue;
-                String fileName=filePath.substring(filePath.lastIndexOf(File.separator)+1);
-                
-                try {
-                	if(!fileName.equals(URLDecoder.decode(fileName,"UTF-8")))
-                    {
-                    	fileName = URLDecoder.decode(fileName,"UTF-8");   
-//                    	System.out.println(fileName);
-                    }
-					fileItem.write(new File(realPath+File.separator+fileName));
-					attachTemp.add(realPath+File.separator+fileName);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}               
+            	if(fileItem.isFormField())
+            	{
+            		String xmString = new String(fileItem.getFieldName().getBytes("UTF-8"));  
+            		String xmlUTF8 = URLDecoder.decode(xmString, "UTF-8");  
+            		String json = xmlUTF8+":"+fileItem.getString("UTF-8");
+//            		System.out.println(json);
+            		JSONObject jsonobj = new JSONObject(json);	
+            	    if(jsonobj.has("sender")){
+            	    	sender = jsonobj.getString("sender");
+            	    }
+            	    if(jsonobj.has("receiver")){
+            	    	receiver = jsonobj.getString("receiver");
+            	    }
+            	    if(jsonobj.has("sendtime")){
+            	    	sendtime = jsonobj.getString("sendtime");
+            	    }
+            	    if(jsonobj.has("subject")){
+            	    	subject = jsonobj.getString("subject");
+            	    }
+            	    if(jsonobj.has("body")){
+            	    	body = jsonobj.getString("body");
+            	    }	
+            	} 
+            	else 
+            	{
+            		String filePath = fileItem.getName();         
+                    if(filePath==null || filePath.trim().length()==0)
+                        continue;
+                    String fileName=filePath.substring(filePath.lastIndexOf(File.separator)+1);
+                    
+                    try {
+                    	if(!fileName.equals(URLDecoder.decode(fileName,"UTF-8")))
+                        {
+                        	fileName = URLDecoder.decode(fileName,"UTF-8");   
+                        }
+    					fileItem.write(new File(realPath+File.separator+fileName));
+    					attachTemp.add(realPath+File.separator+fileName);
+    				} catch (Exception e) {
+    					e.printStackTrace();
+    				}               
+				}             
             }
         }
         catch(FileUploadException e1){
             e1.printStackTrace();
-        }
-        
+        }        
 		email.setSender(sender);
 		email.setReceiver(receiver);
 		email.setSendTime(sendtime);
@@ -179,11 +223,11 @@ public class GetInfo extends HttpServlet {
 	}
 	public EmailInput RevJsonData(EmailInput email,HttpServletRequest request) throws IOException{
 	
-		String sender = null;
-		String receiver = null; 
-        String sendtime = null;
-        String subject = null;
-        String body = null;         
+		String sender = "";
+		String receiver = ""; 
+        String sendtime = "";
+        String subject = "";
+        String body = "";         
         ArrayList<String> attachTemp = new ArrayList<String> ();
         
 		String json = readJSONString(request);
@@ -202,8 +246,7 @@ public class GetInfo extends HttpServlet {
 	    }
 	    if(jsonobj.has("body")){
 	    	body = jsonobj.getString("body");
-	    }
-	    		
+	    }	
 	    email.setSender(sender);
 		email.setReceiver(receiver);
 		email.setSendTime(sendtime);
@@ -212,37 +255,7 @@ public class GetInfo extends HttpServlet {
 		email.setEmailAttach(attachTemp);
 	    return email;
 	}
-	public EmailInput RevFormData(EmailInput email,HttpServletRequest request) throws IOException{
-		
-		String sender = null;
-		String receiver = null; 
-        String sendtime = null;
-        String subject = null;
-        String body = null;         
-        ArrayList<String> attachTemp = new ArrayList<String> ();
-        
-		String formstr = readJSONString(request);
-	    formstr = URLDecoder.decode(formstr, "UTF-8");
-	    String[] tempstr = formstr.split("&");
-	    for(int i=0;i<tempstr.length;i++)
-	    {
-	    	int flag = tempstr[i].indexOf("=");
-	    	tempstr[i] = tempstr[i].substring(flag+1, tempstr[i].length());
-	    }
-	    sender = tempstr[0];
-	    receiver = tempstr[1];
-	    sendtime = tempstr[2];
-	    subject = tempstr[3];
-	    body = tempstr[4];
-	    
-	    email.setSender(sender);
-		email.setReceiver(receiver);
-		email.setSendTime(sendtime);
-		email.setEmailSubject(subject);
-		email.setEmailBody(body);
-		email.setEmailAttach(attachTemp);
-	    return email;
-	}
+	
 	public String readJSONString(HttpServletRequest request) throws IOException
 	{
 		BufferedReader br = new BufferedReader(new InputStreamReader(request.getInputStream(),"UTF-8"));
@@ -252,6 +265,7 @@ public class GetInfo extends HttpServlet {
             sb.append(line);
         }
         String reqBody = sb.toString();
+//      System.out.println(reqBody);
 	    return reqBody;
     }
 	
@@ -287,9 +301,8 @@ public class GetInfo extends HttpServlet {
 				}					
 			}
 		}
-				
-		String filename = fileName;	
-		File file = new File(filename);
+//		System.out.println(fileName);		
+		File file = new File(fileName);
 	    if(!file.exists()) 
 	    {
 		    file.createNewFile();
@@ -323,6 +336,8 @@ public class GetInfo extends HttpServlet {
             }    
             read.close();
         }	
+//		System.out.println(sInput);
+		
 		byte[] temp = sInput.getBytes("UTF-8");
 		sInput = new String(temp,"UTF-8");		
 		String url = "http://111.207.243.70:8839/ibot/api/GetInfo";
@@ -331,7 +346,6 @@ public class GetInfo extends HttpServlet {
    	 	httpclient.executeMethod(method);
    	 	String finalStr = method.getResponseBodyAsString();
    	 	System.out.println(finalStr);
-//   	String finalStr = "{\"city\": \"\",\"company\": \"\",\"equity\": \"\",\"founders\": \"\",\"industry\": \"\",\"money\": \"500万\",\"startup\": \"银驾-模式报告（）\"}";
 		return finalStr;
 
 	}
@@ -351,7 +365,21 @@ public class GetInfo extends HttpServlet {
 			response.setCharacterEncoding("UTF-8");
 			response.setContentType("text/json; charset=UTF-8");
 			response.setHeader("Access-Control-Allow-Origin", "*");			
-			System.out.println("---------------输出ing---------------------");
+			System.out.println("---------------输出ing---------------");
+			response.getOutputStream().write(botResult.getBytes("UTF-8"));			
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public void iBot_Test_Output(String botResult,HttpServletResponse response) throws JSONException
+	{
+		try {		
+			response.reset();
+			response.setCharacterEncoding("UTF-8");
+			response.setContentType("text/plain; charset=UTF-8");
+			response.setHeader("Access-Control-Allow-Origin", "*");			
+			System.out.println("-----------------进入彩蛋-----------------");
 			response.getOutputStream().write(botResult.getBytes("UTF-8"));			
 		}
 		catch (Exception e) {
